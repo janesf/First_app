43409872fa1bf6de763742329179a447cf9b1941
jane <gemini0707@gmail.com>
2012-08-18 10:53:13 -0700

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 FirstApp::Application.routes.draw do
-  root to: 'static_pages#home'
-  match '/', to: 'static_pages#home'
-    match '/help',    to: 'static_pages#help'
-    match '/about',   to: 'static_pages#about'
-    match '/contact', to: 'static_pages#contact'
-    resources :microposts
+  get "static_pages/home"
 
-    resources :users
-    .
-    .
-    .
-  end
+  get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/home"
+  get "static_pages/contact"
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
